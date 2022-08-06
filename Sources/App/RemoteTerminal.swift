2fd022804eccb6fefdacb917396600cebc13c4da
Mateus Rodrigues <mateus.fr26@gmail.com>
2022-08-06 10:17:24 -0300

    Refactor RemoteTerminal

--- Sources/App/RemoteTerminal.swift
+++ Sources/App/RemoteTerminal.swift
@@ -8,30 +8,10 @@
 import Vapor
 
 public class RemoteTerminal: Console {
-    
-    final class Connection: Hashable {
         
-        let id: UUID
-        let socket: WebSocket
-        
-        init(id: UUID = .init(), socket: WebSocket) {
-            self.id = id
-            self.socket = socket
-        }
-        
-        func hash(into hasher: inout Hasher) {
-            hasher.combine(id)
-        }
-        
-        static func == (lhs: Connection, rhs: Connection) -> Bool {
-            lhs.id == rhs.id
-        }
-        
-    }
-    
     public static var `default` = RemoteTerminal()
     
-    var connections = Set<Connection>()
+    private var sockets: [WebSocket] = []
     
     public var size: (width: Int, height: Int) = (0, 0)
     
@@ -40,8 +20,7 @@ public class RemoteTerminal: Console {
     public init() { }
     
     public func connect(_ socket: WebSocket) {
-        let connection = Connection(socket: socket)
-        connections.insert(connection)
+        sockets.append(socket)
     }
     
     public func input(isSecure: Bool) -> String {
@@ -51,8 +30,8 @@ public class RemoteTerminal: Console {
     public func output(_ text: ConsoleText, newLine: Bool) {
         Swift.print(#function, self)
         let data = "\(text)".data(using: .utf8)!
-        connections.forEach {
-            $0.socket.send([UInt8](data))
+        sockets.forEach {
+            $0.send([UInt8](data))
         }
     }
     
