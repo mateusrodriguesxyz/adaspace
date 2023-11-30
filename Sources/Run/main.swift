20eeeaca94c38cb2129ccd55f3aba59d945a5b94
Mateus Rodrigues <mateus.fr26@gmail.com>
2023-11-30 07:55:50 -0300

    Disable RemoteTerminal and FileLogger

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -7,8 +7,8 @@ try LoggingSystem.bootstrap(from: &env) { level in
     return { label in
         MultiplexLogHandler([
             ConsoleLogger(label: label, console: Terminal(), level: level),
-            ConsoleLogger(label: label, console: RemoteTerminal.default, level: level),
-            FileLogger(label: label, filePath: DirectoryConfiguration.detect().publicDirectory + "logs.txt", level: level)
+//            ConsoleLogger(label: label, console: RemoteTerminal.default, level: level),
+//            FileLogger(label: label, filePath: DirectoryConfiguration.detect().publicDirectory + "logs.txt", level: level)
         ])
     }
 }
